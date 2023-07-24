a9c3d1ddaa2d25e559332726713761d04c1e15a7
andre1048576 <55166043+andre1048576@users.noreply.github.com>
2023-07-24 14:00:05 -0400

    removed single page mode

--- main.lua
+++ main.lua
@@ -2,7 +2,6 @@
 -- description: shows what stars are available in each area and which have been collected. Compatible with romhacks that include a template file!
 
 local current_page = 1
-local display_multiple_pages = true
 local star_check_max_pages = -1
 local STAR_CHECK_SCALE = 0.75
 local pt = 15
@@ -16,14 +15,6 @@ local function change_scale(msg)
         return false
     end
     STAR_CHECK_SCALE = num/100
-    return true
-end
-
-local function change_page_count()
-    display_multiple_pages = not display_multiple_pages
-    if current_page == star_check_max_pages then
-        current_page = current_page - 1
-    end
     return true
 end
 
@@ -62,7 +53,7 @@ local function render_page(pageNum,xOffset)
                 djui_hud_set_color(v.r,v.g,v.b,v.a)
             end
         end
-        if (display_multiple_pages and star_check_max_pages > 2) or (not display_multiple_pages and star_check_max_pages > 1) then
+        if star_check_max_pages > 2 then
             djui_hud_set_font(FONT_MENU)
             msg = "<- Page " .. current_page .. " ->"
             textScale = scale/3
@@ -84,7 +75,7 @@ local function on_hud_render()
     right = djui_hud_get_screen_width() - 10*15*STAR_CHECK_SCALE
     djui_hud_set_color(255, 255, 255, 255)
     render_page(current_page,left)
-    if display_multiple_pages and (current_page < star_check_max_pages) then
+    if current_page < star_check_max_pages then
         render_page(current_page+1,right)
         
     end
@@ -92,33 +83,25 @@ end
 ---@param m MarioState
 local function page_control(m)
     if not is_game_paused() then confirm = 0 return end
-    if star_check_max_pages <= 1 then return end
+    if star_check_max_pages <= 2 then return end
     if m.controller.buttonPressed & L_JPAD ~= 0 then
-        if display_multiple_pages then
-            if current_page == 1 then
-                current_page = star_check_max_pages - 1
-            else
-                current_page = current_page - 1
-            end
+        --if you would underflow
+        if current_page <= 1 then
+            current_page = star_check_max_pages - 1
         else
-            if current_page == 1 then
-                current_page = star_check_max_pages
-            else
-                current_page = current_page - 1
-            end
+            current_page = current_page - 1
         end
     elseif m.controller.buttonPressed & R_JPAD ~= 0 then
-        --increase by 1
-        if display_multiple_pages then
-            current_page = current_page % (star_check_max_pages-1) + 1
+        --if you would overflow
+        if star_check_max_pages - current_page <= 1 then
+            current_page = 1
         else
-            current_page = current_page % star_check_max_pages + 1
+            current_page = current_page + 1
         end
     end
 end
 
 hook_event(HOOK_ON_HUD_RENDER, on_hud_render)
 hook_chat_command('scale', "[1 - 100] - adjust the scale of star check (75 is default)", change_scale)
-hook_chat_command('page-count',"changes how many pages of stars are displayed in star check", change_page_count)
 
 hook_event(HOOK_MARIO_UPDATE,page_control)
